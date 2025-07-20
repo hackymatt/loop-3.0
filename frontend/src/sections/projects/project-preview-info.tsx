4c86784dd1c20f32ff0a60a3fe97b638e297933f
loopedupl <admin@loop.edu.pl>
2025-07-20 15:46:07 +0200

    feat: Add ProjectDetailsPreview component for displaying project video previews

--- frontend/src/sections/projects/project-preview-info.tsx
+++ frontend/src/sections/projects/project-preview-info.tsx
@@ -2,7 +2,7 @@ import type { CardProps } from "@mui/material";
 
 import { useTranslation } from "react-i18next";
 
-import { Card } from "@mui/material";
+import { Box, Card } from "@mui/material";
 import Typography from "@mui/material/Typography";
 
 import { Player } from "src/components/player";
@@ -17,7 +17,15 @@ export function ProjectDetailsPreview({ url, sx, ...other }: Props) {
   const { t } = useTranslation("project");
 
   const renderPreview = () => (
-    <Player url={url} playing controls loop muted width="100%" height="100%" />
+    <Box
+      sx={{
+        borderRadius: 2,
+        overflow: "hidden",
+        aspectRatio: "16 / 9",
+      }}
+    >
+      <Player url={url} playing controls loop muted width="100%" height="100%" />
+    </Box>
   );
 
   return (
